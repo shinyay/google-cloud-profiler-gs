842aa58a5a64d1e116b5a51cdfed85428190562d
shinyay <shinya.com@gmail.com>
2021-01-28 23:45:13 +0900

    Initial Commit

---
+++ src/main/java/com/google/shinyay/GoogleCloudProfilerGsApplication.java
@@ -0,0 +1,13 @@
+package com.google.shinyay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudProfilerGsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudProfilerGsApplication.class, args);
+	}
+
+}
